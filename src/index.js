c8b3a9cd5e958eab8c6975ac56d7612bac5843b6
hvb190104 <72687593+hvb190104@users.noreply.github.com>
2021-01-28 01:50:23 +0700

    scss updated

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
 const morgan = require('morgan');
+const { dirname } = require('path');
 const app = express();
 const port = 3000;
 
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+//HTTP logger
+app.use(morgan('combined'));
 
 //Template engine
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, '\\resources\\views'));
-
-//HTTP logger
-app.use(morgan('combined'));
 
 //địa chỉ trang web
 app.get('/', (req, res) => {
